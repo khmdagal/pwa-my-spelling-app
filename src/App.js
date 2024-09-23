7a8e5f617a4f0996ed15c0d1394cb3d8e1c98459
khmdagal <khmdagal@gmail.com>
2024-09-23 18:48:29 +0100

    Change url path based on user authentication, students for student dashboard and teacher for admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import StudentDashboard from './pages/StudentDashboard';
 import HomePage from './pages/Home';
 import SignUp from './pages/SignUp';
 
@@ -16,9 +17,15 @@ function App() {
           <Route exact path='/' element={<HomePage />} />
           <Route exact path='/signup' element={<SignUp />} />
           <Route exact path='/login' element={<Login />} />
-          <Route exact path='/dashboard' element={
+          <Route exact path='/admin-dashboard' element={
             <ProtectedRoutes >
-              <Dashboard />
+              <AdminDashboard />
+            </ProtectedRoutes>
+          }
+          />
+          <Route exact path='/student-dashboard' element={
+            <ProtectedRoutes >
+              <StudentDashboard />
             </ProtectedRoutes>
           }
           />
---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import Time from '../component/Time';
+import GetWords from '../component/Words';
+import Header from '../component/Header';
+//import AssignmentForm from '../forms/WeeklyAssignmentForm';
+import '../css/Dashboard.css'
+
+function Dashboard() {
+    const [yearWords, setYearWords] = useState('year3and4words')
+
+    const handleYearWords = (e) => {
+        e.preventDefault()
+        const selectedYearWords = e.target.value
+        setYearWords(selectedYearWords)
+    }
+    return (
+        <div>
+            <Header />
+            <Time />
+            <h1>Dashboard</h1>
+            <select onChange={handleYearWords}>
+                <option value='year3and4words'>Year 3 and 4 spelling words</option>
+                <option value='year5and6words'>Year 5 and 6 spelling words</option>
+            </select>
+            <GetWords yearWords={yearWords} />
+        </div>
+    )
+
+}
+export default Dashboard;
