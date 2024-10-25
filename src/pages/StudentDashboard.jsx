566e2ce77af51f0534c0516aa15dead70ef15e82
khmdagal <khmdagal@gmail.com>
2024-10-25 17:42:17 +0100

    Modified to match the files changed from assignments folder

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "../component/Header";
+import MyAssignment from "../assignments/myAssignments/MyAssignments";
 
 function StudentDashboard() {
     return (
         <div>
             <h1>Student Dashboard</h1>
             <Header />
+            <MyAssignment />
         </div>
     )
-    
+
 }
 
 export default StudentDashboard;
