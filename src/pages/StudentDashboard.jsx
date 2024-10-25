5074696113dd83be65e965923ac006ff81e64601
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2024-10-25 17:59:31 +0100

    Revert "My assignment component"

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Header from "../component/Header";
-import MyAssignment from "../assignments/myAssignments/MyAssignments";
 
 function StudentDashboard() {
     return (
         <div>
             <h1>Student Dashboard</h1>
             <Header />
-            <MyAssignment />
         </div>
     )
-
+    
 }
 
 export default StudentDashboard;
