6bd4990233ff5dc47690c86574c112ad4529070c
khmdagal <khmdagal@gmail.com>
2024-11-18 13:12:05 +0000

    Add hidden attribute

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,9 +1,9 @@
 
 
-const Button = ({ className, backgroundColor, color, type, label, onClick }) => {
+const Button = ({ className, backgroundColor, color, type, label, onClick, hidden }) => {
 
     return (
-        <button className={className} style={{backgroundColor: backgroundColor,color:color}} type={type} onClick={onClick}>{label}</button>
+        <button className={className} style={{ backgroundColor: backgroundColor, color: color }} type={type} onClick={onClick} hidden={hidden}>{label}</button>
     )
 }
 export default Button
