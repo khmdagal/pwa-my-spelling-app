04d11843cdbb80cd91bf3793c13f3bf0887348e7
khadar m dagal <khmdagal@gmail.com>
2025-05-23 11:49:21 +0100

    Add the header component to the App file

--- src/App.js
+++ src/App.js
@@ -6,41 +6,45 @@ import StudentDashboard from './pages/StudentDashboard';
 import HomePage from './pages/Home';
 import SignUp from './pages/SignUp';
 import PracticeMyAssignment from './assignments/myAssignments/PracticeMyAssignments';
+import Header from './component/Header';
 
 import ProtectedRoutes from './pages/ProtocedRoutes';
 import './css/GlobalStyle.css'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">Spelling Practice App</header>
-        <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route exact path='/signUp' element={<SignUp />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/admin_dashboard' element={
-            <ProtectedRoutes >
-              <AdminDashboard />
-            </ProtectedRoutes>
-          }
-          />
-          <Route exact path='/student_dashboard' element={
-            <ProtectedRoutes >
-              <StudentDashboard />
-            </ProtectedRoutes>
-          }
-          />
-          <Route exact path='/practicePage' element={
-            <ProtectedRoutes >
-              <PracticeMyAssignment />
-            </ProtectedRoutes>
-          } />
-        </Routes>
+    <>
 
-      </div>
-    </Router>
+      
+      <Router>
+        <Header />
+        <div className="App">
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route exact path='/signUp' element={<SignUp />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/admin_dashboard' element={
+              <ProtectedRoutes >
+                <AdminDashboard />
+              </ProtectedRoutes>
+            }
+            />
+            <Route exact path='/student_dashboard' element={
+              <ProtectedRoutes >
+                <StudentDashboard />
+              </ProtectedRoutes>
+            }
+            />
+            <Route exact path='/practicePage' element={
+              <ProtectedRoutes >
+                <PracticeMyAssignment />
+              </ProtectedRoutes>
+            } />
+          </Routes>
 
+        </div>
+      </Router>
+    </>
   );
 }
 
