2a8903e6f937682748f25747006be2e1e8c5f7e5
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2026-02-02 21:10:04 +0000

    Create Profile and handling avatar selections

--- src/component/Avatars.jsx
+++ src/component/Avatars.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { createAvatar } from '@dicebear/core';
 import {
     adventurer, avataaars, adventurerNeutral,
@@ -8,55 +8,21 @@ import {
     micah, miniavs, openPeeps, personas, pixelArt, pixelArtNeutral,
     shapes, thumbs
 } from '@dicebear/collection';
-import { allOtherAxiosRequest } from "../api/axios";
-import { sanitizeInput } from "../helpers/Helpers"
-//import style from '../css/Profile.module.css';
 
-const listAvatas = [adventurer, avataaars, adventurerNeutral,
+const avatarsList = [adventurer, avataaars, adventurerNeutral,
     avataaarsNeutral, bigEars, bigEarsNeutral, bigSmile,
     bottts, botttsNeutral, croodles, croodlesNeutral,
     funEmoji, icons, identicon, lorelei, loreleiNeutral,
     micah, miniavs, openPeeps, personas, pixelArt, pixelArtNeutral,
     shapes, thumbs];
 
-
-
 function Avatars({ avatarName }) {
-    const [errorMessage, setErrorMessage] = useState();
-    const [newAvatarName, setNewAvatarName] = useState('')
-    const [selectedAvatar, setSelectedAvatar] = useState(listAvatas.find((el) => el.meta.title === avatarName));
+    
+    const [selectedAvatar, setSelectedAvatar] = useState(adventurer);
+    useEffect(()=>{
 
-    const myprofile = useMemo(() => {
-        const profile = localStorage.getItem('profile');
-        return  JSON.parse(profile)
-    }, [])
-
-    const updateMyprofile = async () => {
-        try {
-            if (sanitizeInput(newAvatarName)) {
-                setErrorMessage('Invalid input 💪💪')
-                return
-            };
-
-            const response = await allOtherAxiosRequest.put(`/api/v1/spelling/profile/updateProfile/${myprofile?.profile_id}`, { newAvatarName });
-
-            if (response.status === 201) {
-                setSelectedAvatar(listAvatas.find((el) => el.meta.title === newAvatarName))
-            }
-        } catch (error) {
-            console.log(error.message)
-            setErrorMessage(error.message)
-        }
-    };
-
-
-
-    const selectAvatar = (e) => {
-        e.preventDefault()
-        const value = e.target.value
-        setSelectedAvatar(listAvatas.find((el) => el.meta.title === value) || adventurer)
-        setNewAvatarName(value)
-    }
+        setSelectedAvatar(avatarsList.find((el) => el.meta.title === avatarName))
+    },[avatarName])
 
 
     const avatar = useMemo(() => {
@@ -67,27 +33,11 @@ function Avatars({ avatarName }) {
     }, [selectedAvatar]);
 
 
-    const user = localStorage.getItem('user')
-    const userName = JSON.parse(user)?.name
-
-    if (user)
-        return (
-            <div >
-                {errorMessage && <p>{errorMessage}</p>}
-                <img src={avatar} alt="Avatar" />
-                <h2 style={{ color: 'white' }}> {userName} </h2>
-
-                <select onChange={selectAvatar}>
-                    <option value=''>== Select ==</option>
-                    {
-                        listAvatas.map(el => {
-                            return (<option key={el.meta.title} value={el.meta.title}>{el.meta.title}</option>)
-                        })
-                    }
-                </select>
-                <button onClick={updateMyprofile}>Update profile</button>
-            </div>
-        );
+    return (
+        <div >
+            <img src={avatar} alt="Avatar" />
+        </div>
+    );
 
 }
 
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,38 +1,146 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Avatars from "../component/Avatars";
 import { allOtherAxiosRequest } from "../api/axios";
+import { sanitizeInput } from "../helpers/Helpers"
+
+import classes from '../css/Profile.module.css'
 
 
 function UserProfile() {
   const [profile, setProfile] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+  const [newAvatarName, setNewAvatarName] = useState('');
+  const [avatarName, setAvatarName] = useState('Adventurer');
+  const [avatarsList, setAvatarsList] = useState([]);
+  const [hide, setHide] = useState(false);
+  const [showCreateButton, setShowCreateButton] = useState(false)
 
+
+  const userName = useMemo(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return user?.name || ''
+
+  }, [])
 
   useEffect(() => {
-
-    const getMyprofile = async () => {
+    const getAvatars = async () => {
       try {
-        const response = await allOtherAxiosRequest.get(`/api/v1/spelling/profile/getProfile`);
-       
-        if (response.status === 200) {
-         localStorage.setItem('profile', JSON.stringify(response.data.profile[0]))
-          setProfile(response.data.profile[0])
+        const avatars = await allOtherAxiosRequest.get(`/api/v1/spelling/profile/getAvatars`);
+
+        if (avatars.status === 200) {
+          setAvatarsList(avatars.data.avatarNames)
         }
       } catch (error) {
-        console.log(error)
+        console.log(error.message)
+        setErrorMessage(error.message)
       }
+    };
 
+    getAvatars()
+  }, [])
+
+  useEffect(() => {
+    const getMyprofile = async () => {
+      try {
+      
+        const myProfile = await allOtherAxiosRequest.get(`/api/v1/spelling/profile/getProfile`);
+
+        if (myProfile.status === 200) {
+          setProfile(myProfile.data.profile)
+          setAvatarName(myProfile.data.profile.avatar_name)
+          setShowCreateButton(true)
+
+        }
+      } catch (error) {
+        console.log(error.message)
+        setErrorMessage(error.message)
+      }
     };
 
     getMyprofile()
-  }, []);
+  }, [])
 
-  if (profile)
-    return (
-      <div >
-        <Avatars avatarName={profile?.avatar_name} />
+
+  const createMyprofile = async () => {
+    try {
+      if (sanitizeInput(newAvatarName)) {
+        setErrorMessage('Invalid input 💪💪')
+        return
+      };
+
+      const response = await allOtherAxiosRequest.post(`/api/v1/spelling/profile/createProfile`, { newAvatarName });
+
+     
+      if (response.status === 201) {
+        setAvatarName(newAvatarName)
+        setErrorMessage('')
+        setShowCreateButton(true)
+      }
+    } catch (error) {
+      console.log(error)
+      setErrorMessage(error.message)
+    }
+  };
+
+  const updateMyprofile = async () => {
+    try {
+
+      if (sanitizeInput(newAvatarName) || !profile?.profile_id) {
+        setErrorMessage('Invalid input or missing profileId')
+        return
+      };
+
+      const response = await allOtherAxiosRequest.put(`/api/v1/spelling/profile/updateProfile/${profile?.profile_id}`, { newAvatarName });
+
+      if (response.status === 204) {
+        setAvatarName(newAvatarName)
+        setHide(false)
+         setErrorMessage('')
+      }
+    } catch (error) {
+      console.log(error.message)
+      setErrorMessage(error.message)
+    }
+  };
+
+  const handleAvatarName = (e) => {
+    e.preventDefault()
+    const value = e.target.value
+    setAvatarName(value)
+    setNewAvatarName(value)
+
+  }
+
+ 
+
+  return (
+    <div className={`${classes.profile}`}>
+      {errorMessage && <p>{errorMessage}</p>}
+      <div className={`${classes.profileAvatarAndName}`}>
+        <p className={`${classes.profileName}`}>{userName}</p>
+        <div className={`${classes.profileAvatar}`} onClick={()=> setHide((prev)=> !prev)}>
+          <Avatars avatarName={avatarName} />
+        </div>
+
       </div>
-    );
+      {hide && (
+          <div className={`${classes.profileAvatarUpdate}`}>
+        <select onChange={handleAvatarName}>
+          <option value=''>== Select ==</option>
+          {
+            avatarsList?.map(el => {
+              return (<option key={el.avatar_name} value={el.avatar_name}>{el.avatar_name}</option>)
+            })
+          }
+        </select>
+        {showCreateButton ?  <button onClick={updateMyprofile}>Update Avatar</button>:<button onClick={createMyprofile} >Create profile</button>}
+       
+      </div>
+      )}
+
+    </div>
+  );
 
 }
 
-export default UserProfile
+export default UserProfile;
