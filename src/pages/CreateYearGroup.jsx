8dccee9d8d9790ddb5f5fff0276e8932ced75812
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2026-04-08 21:40:16 +0100

    New Spelling Group
    Add functionality to create a new spelling group for a specific year for a specific school.

---
+++ src/pages/CreateYearGroup.jsx
@@ -0,0 +1,82 @@
+import { useState} from 'react';
+import { allOtherAxiosRequest } from '../api/axios';
+import { sanitizeInput } from "../helpers/Helpers";
+
+
+function CreateYearGroup() {
+    const [formData, setFormData] = useState({ group_name: '' });
+    const [errors, setErrors] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+   const years =  JSON.parse(localStorage.getItem('years')) || [];
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }))
+    }
+
+    const rejectEmptyFormSubmission = (formData) => {
+
+        if (Object.values(formData).some((value) => value === '')) {
+            setErrors('Please fill in all field.')
+            return true
+        }
+        return false
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (rejectEmptyFormSubmission(formData)) return
+
+        if (sanitizeInput(formData)) {
+            setErrors('Invalid input 💪💪')
+            return
+        }
+
+        try {
+
+            const response = await allOtherAxiosRequest.post(`/api/v1/spelling/years/createYearGroup/${formData.year_id}`, formData);
+            if (response.status === 201) {
+                setSuccessMessage(response.data.message + ' ✔')
+                setFormData({ year_group_name: '' })
+            }
+        } catch (err) {
+            setErrors(err.response.data.message || 'Failed to create Year Group.');
+        }
+    }
+
+    return (
+        <div>
+            {errors && <p style={{ color: 'red' }}>{errors}</p>}
+            {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+            <h2>Create Year Group</h2>
+            <p>This is where you can create a new year group.</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="year_id">Select Year:</label>
+                <select id="year_id" name="year_id" onChange={handleChange} value={formData.year_id || ''}>
+                    <option value="">--Select Year--</option>
+                    {years.map((year) => (
+                        <option key={year.year_id} value={year.year_id}>
+                            {year.year_name}
+                        </option>
+                    ))}
+                </select>   
+
+                <label htmlFor="year_group_name">Year Group Name:</label>
+                <input
+                    type="text"
+                    id="group_name"
+                    name="group_name"
+                    value={formData.group_name}
+                    onChange={handleChange}
+                />
+                <button type="submit">Create Year Group</button>
+            </form>
+        </div>
+    );
+}
+
+
+
+export default CreateYearGroup;
