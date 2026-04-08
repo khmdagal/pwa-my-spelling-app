924a1d6b55048e7d19675efe6c1ddbb84eb40cd7
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2026-04-08 21:42:27 +0100

    Update LeaderBoard

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -2,13 +2,32 @@ import { useState, useEffect } from "react";
 import { allOtherAxiosRequest } from '../api/axios';
 import { GiTrophyCup, GiPodiumWinner, GiPodiumSecond, GiPodiumThird } from "react-icons/gi";
 
+
 import style from '../css/LeaderBoard.module.css'
 
 
 const Leaderboard = () => {
-  const practice_id = JSON.parse(localStorage.getItem('practice_id'))
   const [leaderboardData, setLeaderboardData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [practiceIds, setPracticeIds] = useState([]);
+  const [practice_id, setPracticeId] = useState(null);
+  const user = localStorage.getItem('user');
+
+  const school_id = user ? JSON.parse(user).school_id : null;
+
+  useEffect(() => {
+    const getAllAssignmentsData = async () => {
+      const response = await allOtherAxiosRequest.get(`/api/v1/spelling/words/weeklypractice/all/${school_id}`);
+
+      if (response.status === 200) {
+        setPracticeIds(response.data.assignments);
+      } else {
+        setPracticeIds([])
+      }
+    }
+    if (school_id) {
+      getAllAssignmentsData()
+    }
+  }, [school_id])
 
   useEffect(() => {
     const getLeaderBoardData = async () => {
@@ -16,21 +35,21 @@ const Leaderboard = () => {
 
         const response = await allOtherAxiosRequest.get(`/api/v1/spelling/leaderBoard/getLeaderBoard/${practice_id}`)
         setLeaderboardData(response.data.result);
-        setLoading(false);
       } catch (error) {
-        console.error("Error fetching leaderboard data: ", error);
-        setLoading(false);
+        console.error("Error fetching leaderboard data");
       }
     }
 
-    getLeaderBoardData()
+    if (practice_id !== null) {
+      getLeaderBoardData();
+    }
 
   }, [practice_id]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
+  const handlePracticeChange = (event) => {
+    setPracticeId(event.target.value);
+  };
 
   const SortedAndfilterdDaa = leaderboardData
     .sort((a, b) => b.total_score - a.total_score)
@@ -39,7 +58,7 @@ const Leaderboard = () => {
 
   const organisePodium = (el, index) => {
     if (index === 0) {
-      return <div key={index}  className={style.eachStudent}>
+      return <div key={index} className={style.eachStudent}>
         <div className={style.podium}>
           <GiTrophyCup color="Gold" />
           <GiPodiumWinner color="gold" size="3em" title="first Icon" />
@@ -57,7 +76,7 @@ const Leaderboard = () => {
       </div>
 
     } else if (index === 1) {
-      return <div key={index}  className={style.eachStudent}>
+      return <div key={index} className={style.eachStudent}>
         <div className={style.podium}>
           <GiPodiumSecond color="Silver" size="3em" title="Second Icon" />
         </div>
@@ -73,7 +92,7 @@ const Leaderboard = () => {
       </div>
     } else if (index === 2) {
 
-      return <div key={index}  className={style.eachStudent}>
+      return <div key={index} className={style.eachStudent}>
 
         <div className={style.podium}>
           <GiPodiumThird color="" size="3em" title="third Icon" />
@@ -88,16 +107,28 @@ const Leaderboard = () => {
         </div>
       </div>
     } else {
-      return ( <div key={index}  className={style.restOfStudents}>
-          <span >{el.name}</span>
-          <span >{el.total_score}</span>
-        </div>)
+      return (<div key={index} className={style.restOfStudents}>
+        <span >{el.name}</span>
+        <span >{el.total_score}</span>
+      </div>)
     }
   }
 
-
   return (
     <div className={`${style.leaderBoard}`}>
+      {practiceIds.length > 0 && (
+        <div className={style.dropdownContainer}>
+          <label htmlFor="practice-select">Select Practice: </label>
+          <select id="practice-select" onChange={handlePracticeChange} value={practice_id || ''}>
+            <option value="" disabled>Select a practice</option>
+            {practiceIds.map((practice) => (
+              <option key={practice.practice_id} value={practice.practice_id}>{`Practice Title: ${practice.assignment.title}`}</option>
+
+            ))}
+          </select>
+        </div>
+      )}
+
       {SortedAndfilterdDaa.map((el, indx) => {
 
         return organisePodium(el, indx)
@@ -105,6 +136,6 @@ const Leaderboard = () => {
       })}
     </div>
   );
-};
+}
 
 export default Leaderboard;
