390e5a51b2d88fddf3d8aa8ed5f9fb4232504a1d
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2026-04-29 14:02:01 +0100

    Update profile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -10,19 +10,27 @@ import classes from '../css/Profile.module.css'
 function UserProfile({ years }) {
   const [profile, setProfile] = useState();
   const [errorMessage, setErrorMessage] = useState();
-  const [newAvatarName, setNewAvatarName] = useState('');
   const [avatarName, setAvatarName] = useState('Adventurer');
-  const [year, setYear] = useState({ year_id: '', year_name: '' });
+  const [newProfile, setNewProfile] = useState({});
+  const [spellingGroup, setspellingGroup] = useState([]);
   const [avatarsList, setAvatarsList] = useState([]);
   const [hide, setHide] = useState(false);
   const [showCreateButton, setShowCreateButton] = useState(false)
 
 
-  const userName = useMemo(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    return user?.name || ''
+  const user = localStorage.getItem('user')
+  const parsedUser = useMemo(() => {
+    return JSON.parse(user) || "";
+  }, [user])
 
-  }, [])
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewProfile((prev) => ({ ...prev, [name]: value }));
+
+    if (name === 'avatarName') {
+      setAvatarName(value);
+    }
+  }
 
   useEffect(() => {
     const getAvatars = async () => {
@@ -46,76 +54,81 @@ function UserProfile({ years }) {
       try {
         const myProfile = await allOtherAxiosRequest.get(`/api/v1/spelling/profile/getProfile`);
 
-        if (myProfile.status === 200) {
-          setProfile(myProfile.data.profile);
-          localStorage.setItem('userProfile', JSON.stringify(myProfile.data.profile));
-          setAvatarName(myProfile.data.profile.avatar_name)
-          setShowCreateButton(true)
-
+        if (myProfile.status !== 200) {
+          setErrorMessage(myProfile.data.message || 'Please Create your profile')
         }
+
+        setProfile(myProfile.data.profile);
+        localStorage.setItem('userProfile', JSON.stringify(myProfile.data.profile));
+        setAvatarName(myProfile.data.profile.avatar_name)
+        setShowCreateButton(true)
+
       } catch (error) {
-        setErrorMessage('An error occurred')
+        console.log(error)
+        setErrorMessage(error.response.data.message || 'An error occurred')
       }
     };
 
     getMyprofile()
   }, [])
 
+  useEffect(() => {
+    const getAllSpellingGroups = async () => {
+      try {
+        const response = await allOtherAxiosRequest.get(`/api/v1/spelling/years/getSpellingGroupsByYear/${newProfile.year_id}`);
+        if (response.status === 200) {
+          setspellingGroup(response.data.allGroups)
+        }
+      } catch (error) {
+        console.log(error.response.data.message || "Spelling groups not found.")
+      }
+    }
+
+    if (newProfile.year_id) {
+      getAllSpellingGroups();
+    }
+
+  }, [newProfile.year_id])
 
   const createMyprofile = async () => {
     try {
-      if (sanitizeInput(newAvatarName)) {
+      if (sanitizeInput(newProfile.avatarName)) {
         setErrorMessage('Invalid input 💪💪')
         return
       };
 
-      const response = await allOtherAxiosRequest.post(`/api/v1/spelling/profile/createProfile`, { newAvatarName, year_id: year.year_id });
+      const response = await allOtherAxiosRequest.post(`/api/v1/spelling/profile/createProfile`, newProfile);
 
       if (response.status === 201) {
-        setAvatarName(newAvatarName)
-        setErrorMessage('')
-        setShowCreateButton(true)
+        setAvatarName(newProfile.avatarName);
+        setErrorMessage('');
+        setShowCreateButton(true);
       }
     } catch (error) {
-      setErrorMessage(error.message)
+      setErrorMessage(error.message || "Unexpected error occured, please try again!");
     }
   };
 
   const updateMyprofile = async () => {
     try {
 
-      if (sanitizeInput(newAvatarName) || !profile?.profile_id) {
-        setErrorMessage('Invalid input or missing profileId')
+      if (sanitizeInput(avatarName) || !profile?.profile_id) {
+        setErrorMessage('Invalid input or missing profileId');
         return
       };
 
-      const response = await allOtherAxiosRequest.put(`/api/v1/spelling/profile/updateProfile/${profile?.profile_id}`, { newAvatarName });
+      const response = await allOtherAxiosRequest.put(`/api/v1/spelling/profile/updateProfile/${profile?.profile_id}`, { avatarName });
 
       if (response.status === 204) {
-        setAvatarName(newAvatarName)
-        setHide(false)
-        setErrorMessage('')
+        setAvatarName(newProfile.avatarName);
+        setHide(false);
+        setErrorMessage('');
       }
     } catch (error) {
-
-      setErrorMessage(error.message)
+      setErrorMessage(error.message || "Unexpected error occured, please try again!");
     }
   };
 
-  const handleAvatarName = (e) => {
-    e.preventDefault()
-    const value = e.target.value
-    setAvatarName(value)
-    setNewAvatarName(value)
-
-  }
-
-  const handleYears = (e) => {
-    e.preventDefault()
-    const value = e.target.value
-    const selectedText = e.target.selectedOptions[0].text;
-    setYear({ year_id: value, year_name: selectedText })
-  }
 
   return (
     <div className={`${classes.profile}`}>
@@ -127,8 +140,8 @@ function UserProfile({ years }) {
         </div>
 
         <div>
-          <p className={`${classes.profileName}`}>{userName}</p>
-          <p className={`${classes.profileName}`}>{profile?.year_name}</p>
+          <p className={`${classes.profileName}`}>{parsedUser?.name || ''}</p>
+          {parsedUser.admin ? "" : <p className={`${classes.profileName}`}>{profile?.year_name}</p>}
         </div>
 
 
@@ -137,7 +150,7 @@ function UserProfile({ years }) {
         <div className={`${classes.profileAvatarUpdate}`}>
           <div className={`${classes.dropDownContainer}`}>
             <label htmlFor="avatarName">Select avatar: </label>
-            <select id="avatarName" onChange={handleAvatarName}>
+            <select name="avatarName" id="avatarName" onChange={handleChange}>
               <option value=''>== Select ==</option>
               {
                 avatarsList?.map(el => {
@@ -147,19 +160,40 @@ function UserProfile({ years }) {
             </select>
           </div>
 
-          {!profile && 
-            <div className={`${classes.dropDownContainer}`}>
-              <label htmlFor="years">Select year: </label>
-              <select  onChange={handleYears} >
-                <option value=''>== Select year ==</option>
-                {
-                  years?.map(el => {
-                    return (<option key={el.year_id} value={el.year_id}>{el.year_name}</option>)
-                  })
-                }
-              </select>
-            </div>}
-          {showCreateButton ? <button onClick={updateMyprofile}>Update Avatar</button> : <button onClick={createMyprofile} >Create profile</button>}
+          {parsedUser.admin ? null : !profile &&
+
+            <div className={`${classes.profileCreationContainer}`}>
+              <div className={`${classes.dropDownContainer}`}>
+                <label htmlFor="years">Select year: </label>
+                <select name="year_id" onChange={handleChange} >
+                  <option value=''>== Select year ==</option>
+                  {
+                    years?.map(el => {
+                      return (<option key={el.year_id} value={el.year_id}>{el.year_name}</option>)
+                    })
+                  }
+                </select>
+              </div>
+
+              <div>
+                <label>Select you spelling spelling group</label>
+                <select name="group_id" onChange={handleChange} required>
+                  <option value=''> Select</option>
+                  {spellingGroup?.map((groups) => {
+                    return (
+                      <option key={groups.group_id} value={groups.group_id}> {groups.group_name} </option>
+                    )
+                  })}
+                </select>
+              </div>
+
+
+            </div>
+
+          }
+          {showCreateButton ?
+            <button onClick={updateMyprofile}>Update Avatar</button> :
+            <button onClick={createMyprofile} >Create profile</button>}
 
         </div>
       )}
